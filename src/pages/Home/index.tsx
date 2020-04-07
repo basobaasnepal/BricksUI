1e9ea3c6b6c82c16d553bd040743805c949170e3
Binaya Rijal <rijalbinaya2@gmail.com>
2020-04-07 21:59:24 +0545

    Adds checkbox page and component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,51 +1,12 @@
 import React from "react";
 import MainLayout from "../../layouts/mainLayout";
-import Input from "../../components/input";
-import Search from "../../components/search";
-import Textarea from "../../components/textarea";
-import { person, search } from "ionicons/icons";
-import { IonIcon } from "@ionic/react";
+import Checkbox from "../../components/checkbox";
 
 const Home = () => {
   return (
     <MainLayout>
-      <div className="title">Home</div>
-      <Input type="password" />
-      <Input
-        prefix={<IonIcon icon={person} />}
-        suffix={<IonIcon icon={search} />}
-      />
-      <Input
-        prefix={<IonIcon icon={person} />}
-        suffix={<IonIcon icon={person} />}
-        preTab="https://"
-      />
-      <Input postTab=".com" preTab="https://" />
-      <Input
-        prefix={<IonIcon icon={person} />}
-        suffix={<IonIcon icon={person} />}
-        preTab="https://"
-        postTab=".com"
-        size="small"
-      />
-      <Input
-        suffix={<IonIcon icon={person} />}
-        preTab="https://"
-        postTab=".com"
-      />
-      <Input suffix={<IonIcon icon={person} />} postTab=".com" />
-      <Input prefix={<IonIcon icon={person} />} preTab="https://" />
-      <Input prefix={<IonIcon icon={person} />} postTab=".com" />
-      <Input size="small" />
-      <Input size="small" />
-      <Input size="large" suffix={<IonIcon icon={person} />} />
-      <Input size="large" />
-      <Search />
-      <Search button="icon" />
-      <Search size="small" button="icon" />
-      <Search size="large" button="icon" />
-      <Search size="large" button="block" />
-      <Textarea />
+      <Checkbox />
+      <div className="checkbox-label">Apple</div>
     </MainLayout>
   );
 };
